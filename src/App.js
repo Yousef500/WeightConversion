4355109761ffdc9bc9a4bb1408e97f8e6537b152
Yousef500 <Kazuya14725@gmail.com>
2022-02-27 05:15:43 +0300

    Added google sign in button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import {Container, CssBaseline, ThemeProvider} from "@mui/material";
 import {createTheme} from "@mui/material/styles";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import {useEffect, useState} from "react";
-import axios from "axios";
 
 const theme = createTheme({
     palette: {
@@ -39,33 +37,9 @@ const theme = createTheme({
     }
 });
 
+
 function App() {
-    const [google, setGoogle] = useState();
 
-    useEffect(() => {
-        const loadGoogle = async () => {
-            const {data} = await axios.get('https://accounts.google.com/gsi/client');
-            setGoogle(data);
-        }
-        loadGoogle();
-
-    }, []);
-
-
-    const handleCredentialResponse = (response) => {
-        console.log("Encoded JWT ID token: " + response.credential);
-    }
-    window.onload = function () {
-        google.accounts.id.initialize({
-            client_id: "915033900039-bdlfua16lgflief48fks1e5u4e6b44u4.apps.googleusercontent.com",
-            callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),
-            {theme: "outline", size: "large"}  // customization attributes
-        );
-        google.accounts.id.prompt(); // also display the One Tap dialog
-    }
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,51 @@
-import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import {AppBar, Grid, Toolbar, Typography} from "@mui/material";
+import GoogleLogin from "react-google-login";
 
 const Header = () => {
+    const client_id = "915033900039-bdlfua16lgflief48fks1e5u4e6b44u4.apps.googleusercontent.com"
+
     return (
-        <AppBar position="static" color="primary" sx={{py: 1, mb: 5}}>
+        <AppBar position="static" color="primary" sx={{pt:1,pb: 1, mb: 5}}>
             <Toolbar>
-                <Typography
-                    variant="h6"
-                    component="div"
-                    sx={{flexGrow: 1, fontWeight: "bold"}}
+                <Grid
+                    alignItems={'center'}
+                    direction={'row'}
+                    container
+                    spacing={1}
                 >
-                    WeightConversion
-                </Typography>
-                <Button color="inherit">
-                    <Typography variant="h7" sx={{fontWeight: "bold"}} id="buttonDiv">
-                        Sign in
-                    </Typography>
-                </Button>
+                    <Grid item xs={8}>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{flexGrow: 1, fontWeight: "bold"}}
+                        >
+                            WeightConversion
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        item xs={4}
+                        display={'flex'}
+                        justifyContent={'right'}
+                        style={{borderWidth: '2px', borderColor: 'green'}}
+                    >
+                        <GoogleLogin
+                            clientId={client_id}
+                            buttonText={'Login with Google'}
+                            onSuccess={(res) => console.log(res)}
+                            onFailure={(res) => console.log(res)}
+                            cookiePolicy={'single_host_origin'}
+                            isSignedIn={true}
+                            style={{marginRight: 0, paddingRight: 0}}
+                        />
+                    </Grid>
+                </Grid>
+
+
+                {/*<Button color="inherit">*/}
+                {/*    <Typography variant="h7" sx={{fontWeight: "bold"}} id="buttonDiv">*/}
+                {/*        Sign in*/}
+                {/*    </Typography>*/}
+                {/*</Button>*/}
             </Toolbar>
         </AppBar>
     );
